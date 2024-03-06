188141d0e3bfd8c30a5013895e08c5ca4af78093
QUANG HUY <QUANG HUY@DESKTOP-HPF1AUU>
2024-03-06 15:30:40 +0700

    6

--- login.php
+++ login.php
@@ -4,5 +4,5 @@ session_start();
 <form action = "login.php" method="post">
     tên đăng nhập:<input type = "text" name="txtUserName"><br>
     mật khẩu: <input type="password" name="txtmatkhau"><br>
-    <input type="btnsubmit" value="login">
+    <input type="btnsubmit" value="Đăng nhập">
 </form>
